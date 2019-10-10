151ba5044918924c40afb94b5a8d7089dbb77b32
Akhil Kestur <akhilkestur@Akhils-MacBook-Pro-2.local>
2019-10-09 23:31:28 -0700

    new at

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -45,8 +45,8 @@ class MoviesController < ApplicationController
   def sorter
     @movies_new = Movie.order(title: :asc)
     @movies = Movie.all
-    @movies.each do |movie|
-      movie.destroy
+    @movies.each do |@movie|
+      @movie.destroy
     @movies.new.each do |movie|
       Movie.create!(movie)
     redirect_to movies_path
