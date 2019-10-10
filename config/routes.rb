78ce1be706082f33586842ac520f3b489da336f9
Akhil Kestur <akhilkestur@Akhils-MacBook-Pro-2.local>
2019-10-09 19:44:23 -0700

    awef

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get 'create' => 'movies#sorter',:as => 'sorter'
+  get 'sorter' => 'movies#sorter',:as => 'sorter'
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
