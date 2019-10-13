6ae6c6ed0bfdf1ba404476f6654f9c6dd2e6471f
Akhil Kestur <akhilkestur@Akhils-MacBook-Pro-2.local>
2019-10-12 19:41:47 -0700

    awef

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -42,6 +42,20 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def sorter
+    @sort_col = Movie.find params[:sort_order]
+    @movies = Movie.order(@sort_col: :asc)
+    # Movie.delete_all
+    # flash[:notice] = @movies
+    @movies.each do |movie|
+      @movie_del = Movie.find(movie['id'])
+      @movie_del.destroy
+      Movie.create!(:title => movie['title'], :rating => movie['rating'], :description => movie['description'], :release_date => movie['release_date'])
+    end
+    redirect_to movies_path
+  end
+
+
   def title_sorter
     @movies = Movie.order(title: :asc)
     # Movie.delete_all
